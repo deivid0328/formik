f5b08a8e111a12a3cee6cb7eb41c7c19195280c1
Deivid Guauña <deividj.54@gmail.com>
2022-05-12 11:56:38 -0500

    inicio de proyecto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 const App = () => {
+
+  const formik = useFormik({
+    initialValues:{
+      name:'',
+      lastname:'',
+      email:'',
+    },
+    onSubmit:values=> console.log(values)
+  })
+
   return (
-    <div>App</div>
+    <from onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name='name'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+    
+    <label>Apellido</label>
+      <input
+        name='lastname'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
+      />
+
+    <label>Nombre</label>
+      <input
+        name='email'
+        type='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button>Enviar</button>
+    </from>
   )
 }
 
